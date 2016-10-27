d19d250edcff757704a2c2bfdd14deed9ba4436d
ShahiM <shahim.m@glossymob.com>
2016-10-27 12:13:29 +0530

    Initial Commit

---
+++ bloodApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bloodApp
+//
+//  Created by GlossyMac2 on 27/10/16.
+//  Copyright © 2016 Unitybees. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
